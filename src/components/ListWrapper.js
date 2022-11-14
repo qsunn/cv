77fa24759ac4bd2985f5ef530065a213c82180a5
Vova Boychyshyn <93547207+qsunn@users.noreply.github.com>
2022-11-14 21:13:43 +0200

    update

--- src/components/ListWrapper.js
+++ src/components/ListWrapper.js
@@ -1,7 +1,7 @@
 export const ListWrapper = ({ children, name, color = 'white' }) => (
     <div className="grid gap-2">
         <h3 className="uppercase font-bold">{name}</h3>
-        <div className={`h-1 bg-${color}`} />
+        <div className={`h-1 ${color === 'black' ? 'bg-black' : 'bg-white'}`} />
         {children}
     </div>
 )
