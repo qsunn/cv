a0eafb3f821890fb72e9f95738178a2c083db758
Vova Boychyshyn <93547207+qsunn@users.noreply.github.com>
2022-11-14 22:02:26 +0200

    update

--- src/components/lists/Info.js
+++ src/components/lists/Info.js
@@ -1,12 +1,16 @@
 import { List } from "../List"
 import { ListWrapper } from "../ListWrapper";
 
+const createLink = href => {
+    return <a href={href === 'GitHub' ? 'https://github.com/qsunn' : 'https://www.linkedin.com/in/boychyshyn/'} target="_blank" rel="noreferrer noopener">{href}</a>
+}
+
 const items = [
     'Lviv, Ukraine',
     '096 808 11 28',
     'qsunnv@gmail.com',
-    'LinkedIn',
-    'GitHub',
+    createLink('LinkedIn'),
+    createLink('GitHub'),
 ];
 
 export const Info = () => <ListWrapper name="Info"><List items={items} /></ListWrapper>;
